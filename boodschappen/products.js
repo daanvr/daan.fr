636cc280565dea581589af469e00fce9208390f6
Daan van Rasmhorst <daanvr@gmail.com>
2021-10-19 21:23:47 +0200

    changes

---
+++ boodschappen/products.js
@@ -0,0 +1,9 @@
+let products = {
+    "Pizzadeeg": {
+        prijs: {
+            prijs: 1.49,
+            winkel: "Lidl",
+            datum: "sept 2021",
+        }
+    }
+}
---
+++ boodschappen/recipes.js
@@ -0,0 +1,41 @@
+let recipies = {
+    name: "Huis gemaakte pizza",
+    description: "",
+    ingredients: {
+        "Deeg": {
+            unit: "rol",
+            quantity: 1,
+            description: "Een deeg met tomaate saus paketje. De tomaten saus gebruiken we niet maar kan in een ander gerecht zo als pasta."
+        },
+        "Gerasptekaas": {
+            unit: "gram",
+            quantity: 150,
+            description: "een zakje jong blegen strooi kaas is het lekkerste."
+        },
+        "Creme fraich": {
+
+        },
+        "Ui": {
+            unit: "",
+            quantity: 0.5,
+        },
+        "Mozerela": {
+            unit: "zakje",
+            quantity: 1,
+        },
+        "Paprika": {
+            unit: "",
+            quantity: 1,
+        },
+        "Champions": {
+            unit: "gram",
+            quantity: 25,
+            optional: true,
+
+        },
+        "Provencaalse kruiden": {
+            optional: true,
+            verySmallQuantity: true,
+        }
+    }
+}
